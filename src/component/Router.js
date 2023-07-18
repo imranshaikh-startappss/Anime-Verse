5df807fe55955cbdb903d2a533a13fa024cc82a5
Anime-Project <imran.shaikh@startappss.com>
2023-07-18 11:56:01 +0530

    redux setup

--- src/component/Router.js
+++ src/component/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "../Header/Navbar";
+// import Navbar from "../Header/Navbar";
+import Login  from "../component/Login/Login";
 const Router = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          {/* <Route path="/" element={<Navbar />} /> */}
+          <Route path="/" element={<Login />}/>
         </Routes>
       </BrowserRouter>
     </>
