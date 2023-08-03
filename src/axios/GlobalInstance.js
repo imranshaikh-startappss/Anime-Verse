8c08f957e18c83676666d208ee1af3609026b195
Gajjuboy <gajendras1997@gmail.com>
2023-08-03 14:48:56 +0530

    test api instance

--- src/axios/GlobalInstance.js
+++ src/axios/GlobalInstance.js
@@ -1,75 +1,146 @@
-import axios from 'axios';
+import axios from "axios";
 
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
-    // Do something before request is sent
-    // console.log(config);
-    return config;
-  }, function (error) {
-    // Do something with request error
-    return Promise.reject(error);
+// export const BaseUrl = [
+//   "https://649177ad2f2c7ee6c2c844a9.mockapi.io",
+//   "https://fakestoreapi.com",
+// ];
+
+// class AxiosBase {
+//   static axiosBase;
+//   constructor(baseURL) {
+//     this.axiosBase = axios.create({ baseURL: baseURL || BaseUrl[1] });
+
+//     // Add a request interceptor
+//     this.axiosBase.interceptors.request.use(
+//       function (config) {
+//         // Do something before request is sent
+//         console.log("Request interceptor: ", config);
+//         return config;
+//       },
+//       function (error) {
+//         // Do something with request error
+//         return Promise.reject(error);
+//       }
+//     );
+
+//     // Add a response interceptor
+//     this.axiosBase.interceptors.response.use(
+//       function (response) {
+//         // Do something with response data
+//         console.log("Response Interceptor: ", response);
+//         return response.data;
+//       },
+//       function (error) {
+//         // Do something with response error
+//         alert("Error Occured");
+//         console.log(`Error Interceptor: ${error}`);
+//         return Promise.reject(error);
+//       }
+//     );
+//   }
+// }
+
+// class AxiosInstance extends AxiosBase {
+
+//   get(url, config) {
+//     return this.axiosBase.get(url, config);
+//   }
+
+//   post(url, body, config) {
+//     return this.axiosBase.post(url, body, config);
+//   }
+
+//   put(url, body, config) {
+//     return this.axiosBase.put(url, body, config);
+//   }
+
+//   patch(url, body, config) {
+//     return this.axiosBase.patch(url, body, config);
+//   }
+
+//   delete(url, config) {
+//     return this.axiosBase.delete(url, config);
+//   }
+// }
+
+
+//  Add a request interceptor
+    axios.interceptors.request.use(
+      function (config) {
+        // Do something before request is sent
+        console.log("Request interceptor: ", config);
+        return config;
+      },
+      function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      }
+    );
+
+    // Add a response interceptor
+    axios.interceptors.response.use(
+      function (response) {
+        // Do something with response data
+        console.log("Response Interceptor: ", response);
+        return response.data;
+      },
+      function (error) {
+        // Do something with response error
+        alert("Error Occured");
+        console.log(`Error Interceptor: ${error}`);
+        return Promise.reject(error);
+      }
+      );
+
+async function MyfetchMiddleWare(url, base="0", options, method = "GET") {
+  console.log("method:", method, "url:", url, "options:", options);
+  let methods = ["get", "post", "put", "patch", "delete"];
+
+  let conditions = methods.map((element) => {
+    return element === method.toLowerCase();
   });
 
-// Add a response interceptor
-axios.interceptors.response.use(function (response) {
-    // Do something with response data
-    console.log("Response Interceptor: ",response);
-    return response.data;
-  }, function (error) {
-    // Do something with response error
-    alert('Error Occured');
-    console.log(`Error Interceptor: ${error}`)
-    return Promise.reject(error);
-  });
+  const APIBase = [
+      "https://649177ad2f2c7ee6c2c844a9.mockapi.io",
+      "https://fakestoreapi.com",
+    ];
 
-
-async function  MyfetchMiddleWare(url,options,method="GET"){
-      console.log("method:",method, "url:",url, "options:",options)
-     let methods = ['get','post','put','patch','delete',]
-
-     let conditions = methods.map(element => {
-      return element === method.toLowerCase()
-     });
 
 
   let commonHeader = {
-    "content-type":"json"
-  }
+    "content-type": "json",
+  };
 
   let CommonBody = {
-    userID:123
-  }
+    userID: 123,
+  };
 
   // let username= localStorage.getitem("username")
 
   // url  = `https://649177ad2f2c7ee6c2c844a9.mockapi.io${url}?username=${username}`
-  let baseUrl  = `https://649177ad2f2c7ee6c2c844a9.mockapi.io${url}`
-  if(options){
+  let baseUrl = `${APIBase[base]}${url}`;
+  if (options) {
+    options.headers = { ...commonHeader, ...options?.headers };
 
-    options.headers = {...commonHeader, ...options?.headers}
-    
-    options.body = {...CommonBody, ...options?.body}
-  }else{
-    options = {headers: {...commonHeader}, body : {...CommonBody}}
+    options.body = { ...CommonBody, ...options?.body };
+  } else {
+    options = { headers: { ...commonHeader }, body: { ...CommonBody } };
   }
 
+  if (JSON.stringify(options.body)) {
+    if (conditions) {
+      let responseData = await axios?.[method?.toLowerCase()](baseUrl, options);
 
+      console.log("Response: ", responseData);
 
-  if(JSON.stringify(options.body)){
-    if(conditions){
-    let responseData = await axios?.[method?.toLowerCase()](baseUrl,options)
-
-    console.log("Response: ",responseData)
-
-    return responseData
-  }else{
-    return false
+      return responseData;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
   }
-}else{
-  return false
 }
 
- 
-}
-
-export default MyfetchMiddleWare
+// export default AxiosInstance;
+export default MyfetchMiddleWare;
--- src/store/slice/Slice.js
+++ src/store/slice/Slice.js
@@ -1,25 +1,57 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import GlobalInstance from "../../axios/GlobalInstance";
 import MyfetchMiddleWare from "../../axios/GlobalInstance";
 
+// const instance = new GlobalInstance();
+
+// export const Product = createAsyncThunk(
+//   "users/fetchByIdStatus",
+//   async (options) => {
+//     try {
+//       let { method, url, base, config } = options;
+
+//       console.log(
+//         "method:",
+//         method,
+//         "base:",
+//         base,
+//         "url:",
+//         url,
+//         "options:",
+//         config
+//       );
+//       return instance.get(url, config);
+//     } catch (error) {
+//       return error;
+//     }
+//   }
+// );
+
 export const Product = createAsyncThunk(
-  "users/fetchByIdStatus", async (options)=>{
-    let {method, url, config} = options
-
-    console.log("method:",method, "url:",url, "options:",config)
-
-
-    console.log("METHOD:",method )
-    // return await MyfetchMiddleWare(url, options, method)
-    // .then(res=>res).catch(error=>error)
-    let res = await MyfetchMiddleWare(url, config,method)
-    try{
-      return {...res}
-    }catch(error){
-      return error
-    }
-    
-  })
+    "users/fetchByIdStatus",
+    async (options) => {
+      try {
+        let { method, url, base, config } = options;
   
+        console.log(
+          "method:",
+          method,
+          "base:",
+          base,
+          "url:",
+          url,
+          "options:",
+          config
+        );
+        return MyfetchMiddleWare(url, base, config, method);
+      } catch (error) {
+        return error;
+      }
+    }
+  );
+
+
+
 const initialState = {
   value: [],
 };
@@ -27,28 +59,27 @@ export const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    getUsers: (state,action) => {
-      console.log("state:", state, "actions:", action)
+    getUsers: (state, action) => {
+      console.log("state:", state, "actions:", action);
     },
   },
-  extraReducers: (builder)=>{
-
-    builder.addCase(Product.pending, ()=>{
-      console.log('loading')
-    })
-    builder.addCase(Product.fulfilled, (state, action)=>{
-      console.log('fullfilled, Storing in state:', action)
-      const {url} = action.meta.arg
-      let key = url.split("").splice(1).join("")
-      console.log(key)
-      state = {...state, [key] : action.payload}
-      return state
-    })
-    builder.addCase(Product.rejected, (state, {error})=>{
-      alert(`Error fetching product details!: ${error.message}`)
-      console.log("Rejected: ", error.message)
-    })
-  }
+  extraReducers: (builder) => {
+    builder.addCase(Product.pending, () => {
+      console.log("loading");
+    });
+    builder.addCase(Product.fulfilled, (state, action) => {
+      console.log("fullfilled, Storing in state:", action);
+      const { url } = action.meta.arg;
+      let key = url.split("").splice(1).join("");
+      console.log(key);
+      state = { ...state, [key]: action.payload };
+      return state;
+    });
+    builder.addCase(Product.rejected, (state, { error }) => {
+      alert(`Error fetching product details!: ${error.message}`);
+      console.log("Rejected: ", error.message);
+    });
+  },
 });
 
 // Action creators are generated for each case reducer function
